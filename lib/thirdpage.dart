bf82535f3c20656761a92abd36822169a1d2e9e4
jac <joshuacarpentier21@gmail.com>
2019-12-06 03:43:15 -0500

    add

---
+++ lib/thirdpage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Third Page',
+          style: TextStyle(fontSize: 25.0, color: Colors.teal),
+        ),
+      ),
+    );
+  }
+}
